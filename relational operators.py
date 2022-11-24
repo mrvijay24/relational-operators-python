b5da95585630df8bfe8e6c1fb12beebf89ad074e
mrvijay24 <110984700+mrvijay24@users.noreply.github.com>
2022-11-24 19:57:39 +0530

    Add files via upload

---
+++ relational operators.py
@@ -0,0 +1,10 @@
+#relational operators
+print('enter the values')
+a=int(input())
+b=int(input())
+print('graterthan:',a>b)
+print('lessthan:',a<b)
+print('graterthan or equalto:',a>=b)
+print('lessthan or equalto:',a<=b)
+print('equals to:',a==b)
+print('not equals to:',a!=b)
